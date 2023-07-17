b238a82d8182f502fdcb9d5804ec83e6416fa5ad
Vinicius Davi <viniprogrammer@hotmail.com>
2023-07-17 20:36:30 -0300

    Criei o arquivo da aula 002

---
+++ Aulas_C++/Aula002.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+/*
+Aula 002: Trabalhando com variáveis e manipulando-as
+*/
+
+using namespace std;
+
+int main(){
+    int n1, n2;
+
+    cout << "Informe dois valores inteiros: ";
+    cin >> n1 >> n2;
+
+    cout << "Os valores informados foram: " << n1 << " e " << n2;
+
+    return 0;
+}
