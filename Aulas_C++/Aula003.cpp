5624372cff705d2be05a20dc5f689fc90fc27a6d
Vinicius Davi <viniprogrammer@hotmail.com>
2023-07-17 21:05:39 -0300

    Adicionei data de nascimento no programa

--- Aulas_C++/Aula003.cpp
+++ Aulas_C++/Aula003.cpp
@@ -7,7 +7,7 @@ Aula003: Trabalhando com multiplas vaariáveis e tipos
 using namespace std;
 
 int main(){
-    int idade;
+    int idade, dia, mes, ano;
     string nome;
     double peso;
 
@@ -17,8 +17,10 @@ int main(){
     cin >> idade;
     cout << "Informe o seu peso em Kg: ";
     cin >> peso;
+    cin >> dia >> mes >> ano;
 
     cout << "Nome: " <<nome << "\n""Idade: " << idade << "\n""Peso: " << peso << endl;
+    cout << "Data de nascimento: " << dia << "/" << mes << "/" <<ano << endl;
     
     return 0;
 }
