a467ce0a40fd68cc96e3711c82cc651461800d44
Vinicius Davi <viniprogrammer@hotmail.com>
2023-07-24 16:36:06 -0300

    Adicionei a Aula012.cpp

---
+++ Aulas_C++/Aula012.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+//Aula012: Trabalhando com matrizees 
+
+using namespace std;
+
+int main(){
+
+    int mat[2][3];
+    int i, j;
+
+    for(i = 0; i < 2; i++){
+        for(j =0; j < 3; j++){
+            cin >> mat[i][j];
+        }
+    }
+
+    
+    for(i = 0; i < 2; i++){
+        for(j =0; j < 3; j++){
+            cout << mat[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
