91e5abf1557e2199f2d446c9b41672f8017fcf75
Vinicius Davi <viniprogrammer@hotmail.com>
2023-07-28 13:22:43 -0300

    Resolvi o exercício de fibonacci recursivo

---
+++ Exercícios/Exercícios/Fibonacci_Recursivo.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int fibo(int x){
+    if(x == 0){
+        return 0;
+    }
+    else if(x == 1){
+        return 1;
+    }   
+    else{
+        return fibo(x - 1) + fibo(x - 2);
+    }
+
+}
+
+int main(){
+    int termo, resposta;
+
+    cin >> termo;
+
+    resposta = fibo(termo);
+
+    cout << resposta << endl;
+
+    return 0;
+}
