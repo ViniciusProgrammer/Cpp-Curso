18fd34ff955d6b00abada6189d5ce3d6ad5fb6b4
Vinicius Davi <viniprogrammer@hotmail.com>
2023-08-04 14:43:16 -0300

    Adicionei a aula031.cpp Introdução a POO

---
+++ Aulas_C++/Aula031.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+/*
+Aula031: Introducão a POO
+*/
+
+using namespace std;
+
+class Aviao{
+
+public:
+    int vel = 0;
+    int velmax;
+    string tipo;
+    void ini(int tp);
+
+private:
+
+};
+
+void Aviao::ini(int tp){
+    if(tp == 1){
+        this -> velmax = 800;
+        this -> tipo = "Jato"; 
+    }
+
+    else if (tp == 2){
+        this -> velmax = 350;
+        this -> tipo = "Monomotor";
+    }
+    else{
+        this -> velmax = 150;
+        this -> tipo = "Planador";
+    }
+}
+
+int main(){
+
+    Aviao *av1 = new Aviao();
+    Aviao *av2 = new Aviao();
+
+    av1 -> ini (1);
+    av2 -> ini (2);
+
+    cout << av1 -> velmax << endl;
+    cout << av2 -> velmax << endl;
+
+    return 0;
+}
